1ed5fea33b661d0082d39bb8688b9ee159a32ef2
Kawsar Hossain <thekawsarhossain@gmail.com>
2023-11-25 21:41:17 +0600

    get and post users endpoint added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,7 +1,7 @@
 import { Schema, model } from 'mongoose';
 import { IUser } from './user.interface';
 import bcrypt from "bcrypt"
-import config from '../app/config';
+import config from '../../config';
 
 const nameSchema = new Schema({
     firstName: { type: String, required: true },
